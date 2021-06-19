cc417ed2fca645ccefc3b36c0b1cf910e8dd41d0
cindyherrera <herreracindy9@gmail.com>
2021-06-19 12:03:05 -0400

    adding intents to MainActivity

--- app/src/main/java/com/example/hydrangeahacks/MainActivity.java
+++ app/src/main/java/com/example/hydrangeahacks/MainActivity.java
@@ -1,21 +1,26 @@
 package com.example.hydrangeahacks;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText user_email, user_password;
     private FirebaseAuth mAuth;
-    private Button btnLogin,btnSignUp;
-
+    private Button btnLogin, btnSignUp;
 
 
     @Override
@@ -38,12 +43,50 @@ public class MainActivity extends AppCompatActivity {
         btnLogin = (Button) findViewById(R.id.btn_login);
         btnSignUp = (Button) findViewById(R.id.btn_signup);
 
-
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
-
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent( MainActivity.this, Signup.class));
+                String email = user_email.getText().toString();
+                final String password = user_password.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //Authenticate user
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //if successful, navigate to HomeScreen
+                        //else display an error message to user
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(MainActivity.this, HomeScreen.class);
+                            startActivity(intent);
+                            finish();
+
+                        } else {
+                            if (password.length() < 8) {
+                                user_password.setError("Password too short--Please enter minimum 8 characters");
+                            } else {
+                                Toast.makeText(MainActivity.this, "Authentication failed", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                });
+                btnSignUp.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(MainActivity.this, Signup.class));
+
+                    }
+                });
 
             }
         });
