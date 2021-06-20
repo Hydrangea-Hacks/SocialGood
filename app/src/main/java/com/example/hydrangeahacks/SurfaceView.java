1b44a3f741f146265b071f2de7b25b2de4d0e391
cindyherrera <herreracindy9@gmail.com>
2021-06-20 04:13:50 -0400

    Surface View & DonationCheck added

---
+++ app/src/main/java/com/example/hydrangeahacks/SurfaceView.java
@@ -0,0 +1,14 @@
+package com.example.hydrangeahacks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SurfaceView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_surface_view);
+    }
+}
